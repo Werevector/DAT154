1e775f801d7cadf50a6bd36f3b90a64b2006ea9f
dysania90 <dysania90@gmail.com>
2016-02-12 14:18:17 +0100

    Made controller with default methods i guess :)

---
+++ Assignment7/Assignment7/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Temp fil for controller, vil fikse på hvor den faktisk skal være osv ... ...
+// Oppsettet er fra view-controller-dao, som betyr at view sender til controller det som er best for view
+// controller tar dette og formaterer det til det som er best for dao, dao sender så videre til databasen
+
+namespace Assignment7
+{
+    class Controller
+    {
+
+
+        public List<Student> searchForStudent (String name)
+        {
+            //fra String til dao passende type ??
+            // dao.searchForStudents(name)
+            // fra dao type tilbake til liste std2, dette er det viktigste denne metoden gjør
+            return null;
+        }
+
+        public List<Student> selectCourse()
+        {
+            //List<Student> std = dao.selectCourse(name);
+
+            return null;
+        }
+
+        public void showGrades()
+        {
+
+        }
+
+        public void ListFailures(String grade)
+        {
+            // hmm, hvordan gjøre denne bedre??
+            //List<Student> std = dao.listStudentsByGrade(grade);
+            //View.displayStudentAttribute(std[i])
+        }
+
+        // View logikk videre her, hvis view ønsker å gjøre noe som krever logikk ...
+
+    }
+}
