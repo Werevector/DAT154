b8a1ef46f80c62d01abee603823a1cde110dae77
Runar Tistel <runartistel@gmail.com>
2016-02-24 04:40:27 +0100

    fixed up the desktop app view

--- SemesterProject/HotelDesktopApp/HotelDesktopApp/Controlls/MainMenu.xaml.cs
+++ SemesterProject/HotelDesktopApp/HotelDesktopApp/Controlls/MainMenu.xaml.cs
@@ -31,39 +31,132 @@ namespace HotelDesktopApp.Controlls
         public string pricing { get; set; }
     }
 
+    public class Reservation
+    {
+        public int rsvr_ID { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public string status { get; set; }
+        public int cstmr_ID { get; set; }
+        public int rsvr_rID { get; set; }
+    }
+
+    public class Customer
+    {
+        public string cstmr_ID { get; set; }
+        public string name { get; set; }
+        public string birthDate { get; set; }
+        public int cardNr { get; set; }
+    }
+
     public partial class MainMenu : UserControl
     {
         public IEnumerable<Room> roomObjects;
+        public IEnumerable<Reservation> reservationObjects;
+        public IEnumerable<Customer> customerObjects;
 
         public MainMenu()
         {
             InitializeComponent();
 
-            string URL = "http://92.221.124.167/rest/listing/rooms";
+            string URL = "http://92.221.124.167/rest/";
+
+            roomDataGrid.AutoGenerateColumns = false;
 
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(URL);
 
-            HttpResponseMessage response = client.GetAsync("").Result;
+            HttpResponseMessage response = client.GetAsync("listing/rooms").Result;
 
             if (response.IsSuccessStatusCode)
             {
-                roomDataGrid.AutoGenerateColumns = false;
                 roomObjects = response.Content.ReadAsAsync<IEnumerable<Room>>().Result;
                 roomDataGrid.ItemsSource = roomObjects;
+            }
 
+            response = client.GetAsync("listing/reservations").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                reservationObjects = response.Content.ReadAsAsync<IEnumerable<Reservation>>().Result;
+            }
+
+            response = client.GetAsync("listing/customers").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                customerObjects = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
             }
 
         }
 
-        private void editBtn_Click(object sender, RoutedEventArgs e)
+        private void roomDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Room selRoom = (Room)roomDataGrid.SelectedItem;
-            if (selRoom != null)
+
+            Reservation resv = null;
+            foreach(var item in reservationObjects)
             {
-                Switcher.Switch(new RoomMenu(selRoom));
+                if(item.rsvr_rID == selRoom.room_ID) { resv = item; }
             }
+
+            if (resv != null) {
+
+                reservedLbl.Content = "True";
+                reservedLbl.Foreground = new SolidColorBrush(Colors.Green);
+
+                isReserved.Header = "True";
+                isReserved.Foreground = new SolidColorBrush(Colors.Green);
+
+                resvDetailName.Content = "Name";
+                resvDetailFrom.Content = resv.startDate.Substring(0,10);
+                resvDetailTo.Content = resv.endDate.Substring(0, 10);
+                resvStatus.Header = resv.status;
+                resvStatus.Foreground = new SolidColorBrush(Colors.Black);
+
+            }
+            else
+            {
+                reservedLbl.Content = "False";
+                reservedLbl.Foreground = new SolidColorBrush(Colors.Red);
+
+                isReserved.Header = "False";
+                isReserved.Foreground = new SolidColorBrush(Colors.Red);
+
+                resvDetailName.Content = "";
+                resvDetailFrom.Content = "";
+                resvDetailTo.Content = "";
+                resvStatus.Header = "";
+            }
+
         }
 
+        private void refreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://92.221.124.167/rest/");
+
+            HttpResponseMessage response = client.GetAsync("listing/rooms").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                roomObjects = response.Content.ReadAsAsync<IEnumerable<Room>>().Result;
+                roomDataGrid.ItemsSource = roomObjects;
+            }
+
+            response = client.GetAsync("listing/reservations").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                reservationObjects = response.Content.ReadAsAsync<IEnumerable<Reservation>>().Result;
+            }
+
+            response = client.GetAsync("listing/customers").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                customerObjects = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+            }
+        }
     }
 }
