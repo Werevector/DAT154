aa97422fc91cb902d4e99d9b36e2e71b9a69d7a3
Runar Tistel <runartistel@gmail.com>
2016-02-26 02:10:10 +0100

    added extra windows for editing

--- SemesterProject/HotelDesktopApp/HotelDesktopApp/Controlls/MainMenu.xaml.cs
+++ SemesterProject/HotelDesktopApp/HotelDesktopApp/Controlls/MainMenu.xaml.cs
@@ -43,26 +43,52 @@ namespace HotelDesktopApp.Controlls
 
     public class Customer
     {
-        public string cstmr_ID { get; set; }
+        public int cstmr_ID { get; set; }
         public string name { get; set; }
         public string birthDate { get; set; }
         public int cardNr { get; set; }
     }
+
+    public class Maintenance
+    {
+        public int mtr_ID { get; set; }
+        public string mtr_Note { get; set; }
+        public string mtr_status { get; set; }
+        public int mtr_room { get; set; }
+    }
+
+    public class Service
+    {
+        public int srv_ID { get; set; }
+        public string note { get; set; }
+        public int room_ID { get; set; }
+    }
+
 
     public partial class MainMenu : UserControl
     {
         public IEnumerable<Room> roomObjects;
         public IEnumerable<Reservation> reservationObjects;
         public IEnumerable<Customer> customerObjects;
+        public IEnumerable<Service> serviceObjects;
+        public IEnumerable<Maintenance> maintenanceObjects;
+
+        public Reservation currentResv = null;
+        public Customer currentCustomer = null;
+        public Room selRoom = null;
+
+        public string URL = "http://92.221.124.167/rest/";
 
         public MainMenu()
         {
             InitializeComponent();
-
-            string URL = "http://92.221.124.167/rest/";
-
             roomDataGrid.AutoGenerateColumns = false;
+            pollRestSrvr();
 
+        }
+
+        private void pollRestSrvr()
+        {
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(URL);
 
@@ -88,74 +114,171 @@ namespace HotelDesktopApp.Controlls
                 customerObjects = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
             }
 
+            response = client.GetAsync("listing/services").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                serviceObjects = response.Content.ReadAsAsync<IEnumerable<Service>>().Result;
+            }
+
+            response = client.GetAsync("listing/maintenance").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                maintenanceObjects = response.Content.ReadAsAsync<IEnumerable<Maintenance>>().Result;
+            }
+
+        }
+
+        private void populateServices(Room room)
+        {
+            foreach (var item in serviceObjects)
+            {
+                if(item.room_ID == room.room_ID)
+                {
+
+                    StackPanel service = new StackPanel();
+                    service.Orientation = Orientation.Vertical;
+                    service.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    service.VerticalAlignment = VerticalAlignment.Stretch;
+
+                    Label lbl = new Label();
+                    lbl.Content = "ID: " + item.srv_ID + " ";
+
+                    TextBlock txtBlock = new TextBlock();
+                    txtBlock.Text = item.note;
+                    txtBlock.TextWrapping = TextWrapping.Wrap;
+
+                    service.Children.Add(lbl);
+                    service.Children.Add(txtBlock);
+
+                    serviceStack.Children.Add(service);
+
+                }
+                
+            }
+           
+        }
+
+        private void populateMaintenance(Room room)
+        {
+            foreach (var item in maintenanceObjects)
+            {
+                if (item.mtr_ID == room.room_ID)
+                {
+
+                    StackPanel maintenance = new StackPanel();
+                    maintenance.Orientation = Orientation.Vertical;
+                    maintenance.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    maintenance.VerticalAlignment = VerticalAlignment.Stretch;
+
+                    Label lbl = new Label();
+                    lbl.Content = "ID: " + item.mtr_ID + " ";
+
+                    TextBlock txtBlock = new TextBlock();
+                    txtBlock.Text = item.mtr_Note;
+                    txtBlock.TextWrapping = TextWrapping.Wrap;
+
+                    maintenance.Children.Add(lbl);
+                    maintenance.Children.Add(txtBlock);
+
+                    maintenanceStack.Children.Add(maintenance);
+
+                }
+
+            }
+        }
+
+        private void clearServices()
+        {
+            serviceStack.Children.Clear();
+        }
+
+        private void clearMaintenance()
+        {
+            maintenanceStack.Children.Clear();
         }
 
         private void roomDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Room selRoom = (Room)roomDataGrid.SelectedItem;
+            selRoom = (Room)roomDataGrid.SelectedItem;
 
-            Reservation resv = null;
-            foreach(var item in reservationObjects)
+            if (selRoom != null)
             {
-                if(item.rsvr_rID == selRoom.room_ID) { resv = item; }
-            }
 
-            if (resv != null) {
+                currentCustomer = null;
+                currentResv = null;
+                clearServices();
+                clearMaintenance();
 
-                reservedLbl.Content = "True";
-                reservedLbl.Foreground = new SolidColorBrush(Colors.Green);
+                foreach (var item in reservationObjects)
+                {
+                    if (item.rsvr_rID == selRoom.room_ID) { currentResv = item; }
+                }
 
-                isReserved.Header = "True";
-                isReserved.Foreground = new SolidColorBrush(Colors.Green);
+                if (currentResv != null)
+                {
 
-                resvDetailName.Content = "Name";
-                resvDetailFrom.Content = resv.startDate.Substring(0,10);
-                resvDetailTo.Content = resv.endDate.Substring(0, 10);
-                resvStatus.Header = resv.status;
-                resvStatus.Foreground = new SolidColorBrush(Colors.Black);
+                    foreach (var item in customerObjects)
+                    {
+                        if (item.cstmr_ID == currentResv.cstmr_ID) { currentCustomer = item; }
+                    }
 
-            }
-            else
-            {
-                reservedLbl.Content = "False";
-                reservedLbl.Foreground = new SolidColorBrush(Colors.Red);
+                    reservedLbl.Content = "True";
+                    reservedLbl.Foreground = new SolidColorBrush(Colors.Green);
 
-                isReserved.Header = "False";
-                isReserved.Foreground = new SolidColorBrush(Colors.Red);
+                    isReserved.Header = "True";
+                    isReserved.Foreground = new SolidColorBrush(Colors.Green);
 
-                resvDetailName.Content = "";
-                resvDetailFrom.Content = "";
-                resvDetailTo.Content = "";
-                resvStatus.Header = "";
+                    resvDetailName.Content = "Name";
+                    resvDetailFrom.Content = currentResv.startDate.Substring(0, 10);
+                    resvDetailTo.Content = currentResv.endDate.Substring(0, 10);
+                    resvStatus.Header = currentResv.status;
+                    resvStatus.Foreground = new SolidColorBrush(Colors.Black);
+                    resvDetailName.Content = currentCustomer.name;
+
+                    populateServices(selRoom);
+                    populateMaintenance(selRoom);
+
+                }
+                else
+                {
+                    reservedLbl.Content = "False";
+                    reservedLbl.Foreground = new SolidColorBrush(Colors.Red);
+
+                    isReserved.Header = "False";
+                    isReserved.Foreground = new SolidColorBrush(Colors.Red);
+
+                    resvDetailName.Content = "";
+                    resvDetailFrom.Content = "";
+                    resvDetailTo.Content = "";
+                    resvStatus.Header = "";
+                    populateMaintenance(selRoom);
+                }
             }
 
         }
 
         private void refreshBtn_Click(object sender, RoutedEventArgs e)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://92.221.124.167/rest/");
+            pollRestSrvr();
+        }
 
-            HttpResponseMessage response = client.GetAsync("listing/rooms").Result;
-
-            if (response.IsSuccessStatusCode)
+        private void customerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentCustomer != null)
             {
-                roomObjects = response.Content.ReadAsAsync<IEnumerable<Room>>().Result;
-                roomDataGrid.ItemsSource = roomObjects;
+                CustomerInfoWindow ciw = new CustomerInfoWindow(currentCustomer);
+                ciw.Show();
             }
+        }
 
-            response = client.GetAsync("listing/reservations").Result;
-
-            if (response.IsSuccessStatusCode)
+        private void EditReservationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(currentResv != null)
             {
-                reservationObjects = response.Content.ReadAsAsync<IEnumerable<Reservation>>().Result;
-            }
-
-            response = client.GetAsync("listing/customers").Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                customerObjects = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+                ReservationEditorWindow rew = new ReservationEditorWindow(currentResv);
+                rew.Show();
             }
         }
     }
---
+++ SemesterProject/HotelDesktopApp/HotelDesktopApp/CustomerInfoWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using HotelDesktopApp.Controlls;
+
+namespace HotelDesktopApp
+{
+    /// <summary>
+    /// Interaction logic for CustomerInfoWindow.xaml
+    /// </summary>
+
+    public partial class CustomerInfoWindow : Window
+    {
+        Customer selectedCustomer = null;
+
+        public CustomerInfoWindow(Customer customer)
+        {
+            selectedCustomer = customer;
+            InitializeComponent();
+            InitFields();
+
+        }
+
+        private void InitFields()
+        {
+            idLbl.Content = selectedCustomer.cstmr_ID;
+            birthLbl.Content = selectedCustomer.birthDate.Substring(0, 10);
+            cardLbl.Content = selectedCustomer.cardNr;
+            nameLbl.Content = selectedCustomer.name;
+        }
+
+        private void editCustmrBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
